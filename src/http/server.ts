d06a20853a6d660ed8563988c3ffeb0c477d5a94
mannuelst <mannuelds@outlook.pt>
2024-02-13 22:40:17 +0100

    update: include zod on project

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,24 @@
+import { PrismaClient } from '@prisma/client';
 import fastify from "fastify";
+import { z } from 'zod';
 
 const app = fastify()
+const prisma = new PrismaClient()
 
-app.post('/polls', (req) => {
-    return 'Hello Server'
+app.post('/polls', async(request, reply) => {
+    const createPoll = z.object({
+        title :z.string()
+    })
+    const { title } = createPoll.parse(request.body)
+    const poll = await prisma.poll.create(
+        {data: 
+            {
+                title
+            }
+        })
+
+   // console.log(request.body.title)
+    return reply.status(201).send({pollid: poll.id})
 })
 
 app.listen({
