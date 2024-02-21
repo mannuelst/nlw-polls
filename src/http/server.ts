2776ec2aa221a786238949d1b5a7e57f3e1ff7d7
mannuelst <manuelds@outlook.pt>
2024-02-21 14:55:32 +0100

    criação dos cookies

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,4 +1,5 @@
 
+import cookie from '@fastify/cookie';
 import fastify from "fastify";
 
 import { createPoll } from "../routes/create-poll";
@@ -8,16 +9,23 @@ import { voteOnPoll } from "../routes/vote-on-poll";
 
 
 const PORT = process.env.PORT || 3333
-// const app = fastify({ logger: true })
-const app = fastify()
+const app = fastify({ logger: true })
+// const app = fastify()
+
+app.register(cookie, {
+    secrete: "polls-app",//um chave!!!
+    hook: "onRequest",
+    //  parseOptions: {}
+})
+
 app.register(createPoll)
 app.register(getPoll)
 app.register(voteOnPoll)
-app.get('/', () => {
-    return 'ROOT ROUT!'
 
-})
 
-app.listen({
-    port: PORT,
-}).then(() => { console.log(`HTTP server is running on ${PORT}!`); })
+// app.get('/', () => {
+//     return 'ROOT ROUT!'
+
+// })
+
+app.listen({ port: PORT }).then(() => { console.log(`HTTP server is running on ${PORT}!`); })
--- src/routes/vote-on-poll.ts
+++ src/routes/vote-on-poll.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from "fastify";
+import { randomUUID } from "node:crypto";
 import { z } from 'zod';
 
 export async function voteOnPoll(app: FastifyInstance) {
     app.post('/polls/:pollId/votes', async (request, reply) => {
+        console.log('inicio')
         const voteOnPollBody = z.object({
             pollOptionId: z.string().uuid()
         })
@@ -13,7 +15,22 @@ export async function voteOnPoll(app: FastifyInstance) {
         const { pollId } = voteOnPollParams.parse(request.params)
         const { pollOptionId } = voteOnPollBody.parse(request.body)
 
-        // console.log(request.body.title)
-        return reply.status(201).send()
+
+        let session_Id = request.cookies.sessionId
+
+        if (!session_Id) { //verificando se já existe uma sessão 
+            session_Id = randomUUID()
+            // console.log({ session_Id })
+            reply.setCookie('sessionId', session_Id, {
+                path: '/',//em quais rotas o cookie estará on
+                maxAge: 60 * 60 * 24 * 30,//30dyas[em segundos], tempo de duração
+                signed: true,
+                httpOnly: true,
+            })
+            console.log('passei!')
+        }
+        console.log('fim', { session_Id })
+
+        return reply.status(201).send(session_Id)
     })
 }
